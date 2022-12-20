2ae73a01c316a45accb66c954b3d84e09155ba6a
Leandro Assis <leandrou9@gmail.com>
2022-12-20 15:53:36 -0300

    Função contadora de ocorrências de termos em arrays (Compass.Uol)

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const numeros = [1, 2, 3, 4];
+const numeros = [1, 2, 3, 4, 1, 1];
 
-const saida = movimenta(numeros, 0, 1);
+const contagem = contaOcorrencias(numeros, 1);
 
-console.log('saida', saida);
+console.log(contagem);
 
-function movimenta(array, index, offset) {
-    const posicao = index + offset;
-    if (posicao >= array.length || posicao < 0) {
-        console.log('Inválido');
-        return;
-    }
-    const saida = [...array];
-    const elemento = saida.splice(index, 1)[0];
-    saida.splice(posicao, 0, elemento);
-    return saida;
+function contaOcorrencias(array, procuraElemento) {
+    /* let contagem = 0
+    for (let elemento of array)
+        if (elemento === procuraElemento)
+            contagem++;
+    return contagem; */
+
+    return array.reduce((acumulador, atual) => {
+        const ocorrencia = (atual === procuraElemento) ? 1 : 0;
+        console.log(acumulador, atual, procuraElemento);
+        return acumulador + ocorrencia;
+    }, 0);
 }
