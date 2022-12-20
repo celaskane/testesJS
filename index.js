7afb8bf91e253b74957f684395011a063c7b172f
Leandro Assis <leandrou9@gmail.com>
2022-12-20 16:09:18 -0300

    Função para filmes

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const numeros = [1, 2, 3, 4];
+const filmes = [
+    { titulo: 'a', ano: 2022, nota: 3.5 },
+    { titulo: 'b', ano: 2022, nota: 3.8 },
+    { titulo: 'c', ano: 2022, nota: 2.5 },
+    { titulo: 'd', ano: 1999, nota: 3.5 },
+];
 
-const max = getMax([]);
+const titulos = filmes
+    .filter(f => f.ano === 2022 && matchMedia.nota >= 3)
+    .sort((a, b) => a.nota - b.nota)
+    .reverse()
+    .map(f => f.titulo)
 
-console.log(max);
-
-function getMax(array) {
-    if (array.length === 0) return undefined;
-
-    /* let max = array[0];
-
-    for (let i = 1; i < array.length; i++)
-        if (array[i] > max)
-            max = array[i];
-    return max; */
-
-    array.reduce((acumulador, atual) => (atual > acumulador) ? acumulador : atual);
-}
+    console.log('titulos', titulos)
