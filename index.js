bc10b356849e4505f8562de0d9bc4335245c4b53
Leandro Assis <leandrou9@gmail.com>
2022-12-20 12:02:20 -0300

    Criação de objetos com arrays (Compass.Uol)

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-const blog = {
-    titulo: 'a',
-    corpo: 'b',
-    autor: 'c',
-    visualizacoes: 1,
-    comentarios: [
-        {autor: 'ab', corpo: 'qq'},
-        {autor: 'bbb', corpo: 'pppp'}
-    ],
-    taOn: true
-};
-console.log('blog', blog);
+let precosValores = [
+    { marca: '$', dica: 'barato', minPorPessoa: 0, maxPorPessoa: 10 },
+    { marca: '$$', dica: 'moderado', minPorPessoa: 11, maxPorPessoa: 20 },
+    { marca: '$$$', dica: 'caro', minPorPessoa: 21, maxPorPessoa: 50 }
+];
 
-function Postagem(titulo, corpo, autor) {
-    this.titulo = titulo;
-    this.corpo = corpo;
-    this.autor = autor;
-    this.visualizacoes = 0;
-    this.comentarios = [];
-    this.taOn = false;
-}
-
-let postagem = new Postagem('a', 'b', 'c');
-console.log('posagem', postagem);
+let restaurantes = [
+    { mediaPorPessoa: 5 }
+];
