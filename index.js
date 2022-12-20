a91ada706257e2bb83652957e022a67c32adcd9c
Leandro Assis <leandrou9@gmail.com>
2022-12-20 15:40:07 -0300

    Função para movimentar elementos em uma array (Compass.Uol)

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const numeros = [1, 2, 3, 4];
 
-const saida = excessao(numeros, [1]);
+const saida = movimenta(numeros, 0, 1);
 
 console.log('saida', saida);
 
-function excessao(array, excluido) {
-    const saida = [];
-    for (let elemento of array)
-        if (!excluido.includes(elemento))
-            saida.push(elemento);
+function movimenta(array, index, offset) {
+    const posicao = index + offset;
+    if (posicao >= array.length || posicao < 0) {
+        console.log('Inválido');
+        return;
+    }
+    const saida = [...array];
+    const elemento = saida.splice(index, 1)[0];
+    saida.splice(posicao, 0, elemento);
     return saida;
 }
