c0768db674b41bdfd1956d23728af30889ed8ea8
Leandro Assis <leandrou9@gmail.com>
2022-12-20 11:38:28 -0300

    Comparando igualdade entre objetos (Compass.Uol)

--- index.js
+++ index.js
@@ -22,5 +22,18 @@ function MostraLocalizacao(rua, cidade, cep) {
     this.cep = cep;
     
 }
-const endereco = new MostraLocalizacao('1', '2', '3');
-console.log('endereco', endereco);
+const endereco1 = new MostraLocalizacao('1', '2', '3');
+const endereco2 = new MostraLocalizacao('1', '2', '3');
+
+function saoIguais(endereco1, endereco2) {
+    return endereco1.rua === endereco2.rua &&
+    endereco1.cidade === endereco2.cidade &&
+    endereco1.cep === endereco2.cep;
+}
+
+function saoIdenticos(endereco1, endereco2) {
+    return endereco1 === endereco2;
+}
+
+console.log('saoIguais?', saoIguais(endereco1, endereco2));
+console.log('saoIdenticos?', saoIdenticos(endereco1, endereco2));
